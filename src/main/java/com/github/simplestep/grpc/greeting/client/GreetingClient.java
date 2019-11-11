86586198cd88e47657354096787f8a7f7acee022
王權 <timowang@wangquande-MacBook-Pro.local>
2019-11-11 23:02:50 +0800

    init commit

---
+++ src/main/java/com/github/simplestep/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,146 @@
+package com.github.simplestep.grpc.greeting.client;
+
+//import com.proto.dummy.DummyServiceGrpc;
+//import com.proto.greet.GreetEveryoneRequest;
+//import com.proto.greet.GreetEveryoneResponse;
+//import com.proto.greet.GreetManyTimesRequest;
+//import com.proto.greet.GreetRequest;
+//import com.proto.greet.GreetResponse;
+//import com.proto.greet.GreetServiceGrpc;
+//import com.proto.greet.GreetWithDeadlineRequest;
+//import com.proto.greet.GreetWithDeadlineResponse;
+//import com.proto.greet.Greeting;
+//import com.proto.greet.LongGreetRequest;
+//import com.proto.greet.LongGreetResponse;
+import io.grpc.Deadline;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+
+import javax.net.ssl.SSLException;
+import java.io.File;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello gRPC client");
+
+        GreetingClient main = new GreetingClient();
+        main.run();
+    }
+
+    public void run() {
+        ManagedChannel channel = null;
+
+//        doUnaryCall(channel);
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+
+    /*
+    private void doUnaryCall(ManagedChannel channel) {
+        // TODO
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = null;
+
+        // Unary
+        // TODO: created a protocol buffer greeting message
+        Greeting greeting = null;
+
+        // TODO: do the same for a GreetRequest
+        GreetRequest greetRequest = null;
+
+        // TODO: call the RPC and get back a GreetResponse (protocol buffers)
+
+//        System.out.println("greetResponse : " + greetResponse.getResult());
+    }
+     */
+
+    /*
+    private void doServerStreamingCall(ManagedChannel channel) {
+        // TODO
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = null;
+
+        // Server Streaming
+        // TODO: we prepare the request
+        GreetManyTimesRequest greetManyTimesRequest = null;
+
+        // TODO: we stream the responses (in a blocking manner)
+    }
+
+     */
+
+    /*
+    private void doClientStreamingCall(ManagedChannel channel) {
+        // TODO: create an asynchronous client
+        GreetServiceGrpc.GreetServiceStub asyncClient = null;
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        // TODO
+        StreamObserver<LongGreetRequest> requestObserver = null;
+
+        // streaming message #1
+        System.out.println("sending message #1");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("Stephane").build())
+                .build());
+
+        // streaming message #2
+        System.out.println("sending message #2");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("John").build())
+                .build());
+
+        // streaming message #3
+        System.out.println("sending message #3");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("Marc").build())
+                .build());
+
+        // we tell the server taht the client is done sending data
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3L, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+     */
+
+    /*
+    private void doBiDiStreaming(ManagedChannel channel) {
+        GreetServiceGrpc.GreetServiceStub asyncClient = GreetServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<GreetEveryoneRequest> requestObserver = asyncClient.greetEveryone(null);
+
+        Arrays.asList("Stephane", "John", "Marc", "Patricia").forEach(name -> {
+            System.out.println("Sending: " + name);
+            requestObserver.onNext(GreetEveryoneRequest.newBuilder()
+                    .setGreeting(Greeting.newBuilder().setFirstName(name).build()).build());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3L, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+     */
+}
---
+++ src/main/java/com/github/simplestep/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,27 @@
+package com.github.simplestep.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        // TODO: plaintext server
+        Server server = null;
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received Shutdown Request");
+            server.shutdown();
+            System.out.println("Successfully stopped the server");
+        }));
+
+        server.awaitTermination();
+    }
+}
