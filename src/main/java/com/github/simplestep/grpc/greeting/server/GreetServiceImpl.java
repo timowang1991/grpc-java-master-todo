7420278bf49416289c2085cda28dfbd2fb55a842
王權 <timowang@wangquande-MacBook-Pro.local>
2019-11-18 21:31:08 +0800

    remove all in GreetServiceImpl

--- src/main/java/com/github/simplestep/grpc/greeting/server/GreetServiceImpl.java
+++ src/main/java/com/github/simplestep/grpc/greeting/server/GreetServiceImpl.java
@@ -1,58 +1,6 @@
 package com.github.simplestep.grpc.greeting.server;
 
-//import com.proto.greet.GreetEveryoneRequest;
-//import com.proto.greet.GreetEveryoneResponse;
-//import com.proto.greet.GreetManyTimesRequest;
-//import com.proto.greet.GreetManyTimesResponse;
-//import com.proto.greet.GreetRequest;
-//import com.proto.greet.GreetResponse;
-//import com.proto.greet.GreetServiceGrpc;
-//import com.proto.greet.GreetWithDeadlineRequest;
-//import com.proto.greet.GreetWithDeadlineResponse;
-//import com.proto.greet.Greeting;
-//import com.proto.greet.LongGreetRequest;
-//import com.proto.greet.LongGreetResponse;
-import io.grpc.Context;
-import io.grpc.stub.StreamObserver;
-
 // TODO: extend
 public class GreetServiceImpl {
 
-    /*
-    @Override
-    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
-        // TODO: extract the fields we need
-
-        // TODO: create the response
-
-        // TODO: send the response
-
-        // TODO: complete the RPC call
-    }
-     */
-
-    /*
-    @Override
-    public void greetManyTimes(GreetManyTimesRequest request, StreamObserver<GreetManyTimesResponse> responseObserver) {
-        // TODO
-    }
-
-     */
-
-    /*
-    @Override
-    public StreamObserver<LongGreetRequest> longGreet(StreamObserver<LongGreetResponse> responseObserver) {
-        // TODO
-        return null;
-    }
-
-     */
-
-    /*
-    @Override
-    public StreamObserver<GreetEveryoneRequest> greetEveryone(StreamObserver<GreetEveryoneResponse> responseObserver) {
-        // TODO
-        return null;
-    }
-     */
 }
